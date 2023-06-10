c6900fbbf0b937774880c6a8c7831c0472d3b080
kknni <gasiorowicz10@gmail.com>
2023-06-10 19:21:25 +0900

    zustand설치, Users Store 생성

--- src/components/contents/Users.tsx
+++ src/components/contents/Users.tsx
@@ -1,8 +1,43 @@
+import { usersStore } from '../../stores/usersStore';
+
 function Users() {
+  const usersState = usersStore((state) => state);
+  const user = usersState.user;
+  const users = usersState.users;
+  console.log(user, users);
   return (
     <div>
       <h3>Users</h3>
-      <p>Contents</p>
+      <hr className="d-block" />
+      <div>
+        <h4>Read</h4>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Modify</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>홍길동</td>
+              <td>20</td>
+              <td>
+                <button>Update</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <hr className="d-block" />
+      <div>
+        <h4>Create</h4>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Age" />
+        <button>Create</button>
+      </div>
     </div>
   );
 }
